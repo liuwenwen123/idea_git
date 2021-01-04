24a4de1aaab0dfd5c4bd050c8f49e93419888fb1
liuwenwen123 <jsj_liuwenwen@163.com>
2021-01-04 23:46:53 +0800

    moidfy TemstMain

--- ideatesttest/src/main/java/com/liuwenwen/TestMain.java
+++ ideatesttest/src/main/java/com/liuwenwen/TestMain.java
@@ -1,7 +1,9 @@
 package com.liuwenwen;
 
+import java.util.ArrayList;
+
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("say my world");
+        System.out.println("say test1 world");
     }
 }
