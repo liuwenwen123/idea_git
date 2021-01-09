0a52766b482f324497d3de8d9b04243969fdedef
liuwenwen123 <jsj_liuwenwen@163.com>
2021-01-09 15:11:00 +0800

    modify TestmainRename

--- ideatesttest/src/main/java/com/liuwenwen/TestMainRename.java
+++ ideatesttest/src/main/java/com/liuwenwen/TestMainRename.java
@@ -7,9 +7,7 @@ import java.util.List;
 public class TestMainRename {
     public static void main(String[] args) {
         System.out.println("say test2 world");
-
         System.out.println("add");
-
         System.out.println("new tel");
     }
 }
